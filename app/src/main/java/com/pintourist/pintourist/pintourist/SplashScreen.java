a82ba32cdc4fba8e87f6b6610c1d3ae3947ad6f4
fedebyes <fedeb703@gmail.com>
2017-04-04 14:02:04 +0200

    Added  splash and login activity

---
+++ app/src/main/java/com/pintourist/pintourist/pintourist/SplashScreen.java
@@ -0,0 +1,74 @@
+package com.pintourist.pintourist.pintourist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreen extends AppCompatActivity {
+    public static final int SPLASH_ACTIVITY_TIME = 4000;
+    private static final String TAG = "SplashActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                // Start the next activity
+
+                Intent mainIntent = new Intent().setClass(
+                        SplashScreen.this, MainActivity.class);
+                mainIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                //mainIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(mainIntent);
+                mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                // Close the activity so the user won't able to go back this
+                // activity pressing Back button
+                finish();
+
+                /*if (currentUser != null) {
+
+                    FacebookProfile.getGraphRequest(ParseUser.getCurrentUser());
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    finish();
+
+                } else {
+                    // show the signup or login screen
+
+                    Intent mainIntent = new Intent().setClass(
+                            SplashActivity.this, DefaultIntro.class);
+                    mainIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    //mainIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(mainIntent);
+                    mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    // Close the activity so the user won't able to go back this
+                    // activity pressing Back button
+                    finish();
+                    progressWheel.stopSpinning();
+                    //progressWheel.setVisibility(View.INVISIBLE);
+                }
+*/
+            }
+        };
+        Timer timer = new Timer();
+
+        timer.schedule(task, SPLASH_ACTIVITY_TIME);
+
+
+    }
+
+}
