99cc35748e9c97319f4b811c6efc45654a9bfd11
fedebyes <fedeb703@gmail.com>
2017-04-07 17:16:21 +0200

    Maps Work Permission not work

--- app/src/main/java/com/pintourist/pintourist/pintourist/MapsFragment.java
+++ app/src/main/java/com/pintourist/pintourist/pintourist/MapsFragment.java
@@ -1,18 +1,25 @@
 package com.pintourist.pintourist.pintourist;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 
 import static android.content.ContentValues.TAG;
 
@@ -25,17 +32,21 @@ import static android.content.ContentValues.TAG;
  * Use the {@link MapsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MapsFragment extends SupportMapFragment {
+public class MapsFragment extends SupportMapFragment implements OnMapReadyCallback {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    
+    private String[] PERMISSIONS = new String[]{ Manifest.permission.,
+            Manifest.permission.ACCESS_COARSE_LOCATION};
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+    private GoogleMap googleMap;
 
     public MapsFragment() {
         // Required empty public constructor
@@ -66,25 +77,14 @@ public class MapsFragment extends SupportMapFragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-        getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(GoogleMap googleMap) {
-                Log.d(TAG, "Map Ready");
-            }
-        });
-        /*SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);*/
+        getMapAsync(this);
+
+
     }
 
 
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_maps, container, false);
-    }
+
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
@@ -110,6 +110,33 @@ public class MapsFragment extends SupportMapFragment {
         mListener = null;
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap=googleMap;
+        setUpMap(googleMap);
+        Log.d(TAG,"Maps Ready");
+
+
+
+
+    }
+
+    private void setUpMap(GoogleMap googleMap) {
+
+        googleMap.getUiSettings().setMapToolbarEnabled(true);
+        googleMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+        //permissionRequest();
+        LatLng ROMA = new LatLng(41.9000, 12.5000);
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(ROMA)      // Sets the center of the map to mi position
+                .zoom(5)                   // Sets the zoom
+                .bearing(0)                // Sets the orientation of the camera to east
+                .build();                   // Creates a CameraPosition from the builder
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+
+    }
 
 
     /**
@@ -125,5 +152,53 @@ public class MapsFragment extends SupportMapFragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        setUpMapIfNeeded();
+
+    }
+
+    private void setUpMapIfNeeded() {
+        if (googleMap == null) {
+            getMapAsync(new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(GoogleMap gMap) {
+                    googleMap = gMap;
+                    setUpMap(googleMap);
+                    gMap.getUiSettings().setMapToolbarEnabled(true);
+                    permissionRequest();
+
+                }
+            });
+        }
+    }
+
+    private void permissionRequest() {
+
+        //googleMap.setMyLocationEnabled(true);
+        if (Build.VERSION.SDK_INT < 23) {
+            if(googleMap!=null){
+                googleMap.setMyLocationEnabled(true);
+            }
+            //Log.d(TAG, "sdk<23");
+        } else {
+            int permissionCheck = ContextCompat.checkSelfPermission(getActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION);
+            if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+                //Log.d(TAG, "permission granted on check");
+                if(googleMap!=null)googleMap.setMyLocationEnabled(true);
+            } else {
+                // request permission.
+                ActivityCompat.requestPermissions(getActivity(), PERMISSIONS, MY_PERMISSIONS_REQUEST_LOCATION);
+                //Log.d(TAG, "Requesting permission " + MY_PERMISSIONS_REQUEST_LOCATION);
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
     }
 }
